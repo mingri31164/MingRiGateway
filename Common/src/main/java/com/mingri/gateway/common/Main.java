ac91e05773aaf8d506eac647bd2b0bb4832f832b
mingri31164 <3116430062@qq.com>
2025-03-23 02:44:08 +0800

    feat：初始化项目架构

---
+++ Common/src/main/java/com/mingri/gateway/common/Main.java
@@ -0,0 +1,7 @@
+package com.mingri.gateway.common;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Core/src/main/java/com/mingri/gateway/core/Main.java
@@ -0,0 +1,7 @@
+package com.mingri.gateway.core;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
